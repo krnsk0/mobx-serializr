1ea5ad273bb865a202eb30e9bdca345c66d404a1
krnsk0 <jkurinsky@gmail.com>
2021-03-16 22:49:17 -0400

    wip

--- src/index.ts
+++ src/index.ts
@@ -2,24 +2,34 @@ import { deserialize, serialize } from 'serializr';
 import { RootStore } from './rootStore';
 
 const root = new RootStore();
-root.fooStore.incrementCounters();
-root.fooStore.incrementCounters();
-root.fooStore.incrementCounters();
 
+// set some state and make an assertion about it
+root.fooStore.incrementCounters();
+root.fooStore.incrementCounters();
+root.fooStore.incrementCounters();
 const firstCounterSum = root.fooStore.counterSum;
 console.assert(firstCounterSum === 9);
 
+// attempt to serialize the root
 console.log('serializing...');
 const json = serialize(RootStore, root);
 
+// attempt to deserialize
 console.log('deserializing...');
 const deserialized = deserialize(
   RootStore,
   json,
-  (err, user) => console.log('done'),
+  (err) => {
+    if (err) console.error(err);
+  },
   //context object
   { rootStore: new RootStore() }
 );
 
+// check to see if state was preserved
 const secondCounterSum = deserialized.fooStore.counterSum;
 console.assert(secondCounterSum === 9);
+
+// check each foo's reference to root
+console.log(deserialized.uuid);
+console.log(deserialized.fooStore.foos[0].root.uuid);
